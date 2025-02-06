6d88f05bc179eed7aca3637b747ce20639ebceef
Aaqib Mir <67498640+whokashmiri@users.noreply.github.com>
2025-02-06 11:55:07 +0530

    Toggle like

--- social/src/actions/post.action.ts
+++ social/src/actions/post.action.ts
@@ -5,6 +5,7 @@ import { getDbUserId } from "./user.action"
 import { revalidatePath } from "next/cache";
 
 
+
 export async function createPost(content:string , image:string) {
     try {
         
@@ -51,16 +52,92 @@ export async function getPosts() {
                                 name:true
                             }
                         }
-                    }
+                    },
+                    orderBy:{
+                        createdAt:"asc"
+                    },
                 },
+               
                 likes:{
                 select:{
-    userId:true
+                    userId:true
+                    }
+                },
+                _count:{
+                    select:{
+                        likes:true,
+                        comments:true
                     }
                 }
             }
         })
+        return posts
     } catch (error) {
+        console.log("Error in Get Posts" , error);
+        throw new Error("Failed to fetch posts")
+        
+        
+    }
+    
+}
+
+export async function toggleLike(postId:string) {
+    try {
+        const userId= await getDbUserId();
+        if(!userId) return
+        const existingLke = await prisma.like.findUnique({
+            where:{
+                userId_postId:{
+                    userId,
+                    postId,
+                }
+            }
+        })
+        const post  = await prisma.post.findUnique({
+            where:{id:postId},
+            select:{authorId:true},
+        });
+
+        if(!post) throw new Error("Post Not Found");
+
+        if(existingLke){
+            await prisma.like.delete({
+                where:{
+                    userId_postId:{
+                        userId,
+                        postId,
+                    },
+                },
+            });
+        }else{
+            await prisma.$transaction([
+                prisma.like.create({
+                    data:{
+                        userId,
+                        postId,
+                    },
+                }),
+                ...(post.authorId!==userId
+                    ?[
+                        prisma.notification.create({
+                            data:{
+                                type:"LIKE",
+                                userId:post.authorId,
+                                creatorId:userId,
+                                postId,
+                            }
+                        })
+                    ]
+                    :[]
+                ),
+            ]);
+        }
+        revalidatePath("/")
+        return{success:true};
+    } catch (error) {
+        console.log("Failed to toggle like : " , error);
+        return{success: false , error:"Failed to toggle like"};
+        
         
     }
     
