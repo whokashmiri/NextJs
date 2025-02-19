8cb6b0833dad26a0147671df68b5f47fe6406c5f
Aaqib Mir <67498640+whokashmiri@users.noreply.github.com>
2025-02-19 22:20:04 +0530

    Almost Done

--- social/src/app/layout.tsx
+++ social/src/app/layout.tsx
@@ -19,8 +19,8 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Socially",
-  description: "A modern social media application powered by Next.js",
+  title: "Social",
+  description: "A  social media web app powered by Next.js",
 };
 
 export default function RootLayout({
